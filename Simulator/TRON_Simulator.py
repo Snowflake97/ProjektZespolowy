b3781df4ee7e309cf23b4e1b79056cc12e1ef875
snowflake97 <235984@student.pwr.edu.pl>
2020-04-17 13:36:53 +0200

    - minor changes in simulator (static path to python interpreter replaced by os.path) - yellow representation -> red (front-end)

--- Simulator/TRON_Simulator.py
+++ Simulator/TRON_Simulator.py
@@ -7,12 +7,18 @@ import django
 django.setup()
 
 from Simulator.Communication import *
-import time, random
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PYTHON_INTERPRETER_DIR = os.path.join(BASE_DIR, "venv", "bin", "python")
 
 gameboard = Gameboard()
 
-bot_1 = pexpect.spawn(f"/home/adi/venv/bin/python bot.py bot_1")
-bot_2 = pexpect.spawn(f"/home/adi/venv/bin/python bot.py bot_2")
+# for this to work u should have virtual enviroment in ProjektZespolowy main directiory (same level as ProjektZespolowy, Simulator, tron_app etc)
+# example: */ProjektZespolowy/venv/bin/python
+# this might change
+
+bot_1 = pexpect.spawn(f"{PYTHON_INTERPRETER_DIR} bot.py bot_1")
+bot_2 = pexpect.spawn(f"{PYTHON_INTERPRETER_DIR} bot.py bot_2")
 for i in range(20):
     row, col = whole_seq(bot_1)  # sequence for bot1(get position and direction, send map to bot and mark move)
     if gameboard.check_if_colision(row,col):
