1f423b9a7ed40deabd5c004a5cb351aa30a9189b
snowflake97 <235984@student.pwr.edu.pl>
2020-04-17 20:14:53 +0200

    - add and run *.js bot

--- Simulator/TRON_Simulator.py
+++ Simulator/TRON_Simulator.py
@@ -9,32 +9,36 @@ django.setup()
 from Simulator.Communication import *
 
 
-
-
 def run():
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    # */ProjektZesplowy/venv/bin/python (might change)
+    # PYTHON INTERPRETER */ProjektZesplowy/venv/bin/python (might change)
     PYTHON_INTERPRETER_DIR = os.path.join(BASE_DIR, "venv", "bin", "python")
+    # NODEJS INTERPRETER (sudo-apt get install nodejs)
+    NODE_INTERPRETER_DIR = "/usr/bin/nodejs"
+
+    # */Projektespolowy/Simulator/bot.js (will change for sure)
+    BOT_JS_DIR = "/home/adi/Desktop/DjangoProjects/ProjektZespolowy/Simulator/bot.js"
     # */Projektespolowy/Simulator/bot.py (will change for sure)
-    BOT_DIR = os.path.join(BASE_DIR, "Simulator", "bot.py")
+    BOT_PY_DIR = os.path.join(BASE_DIR, "Simulator", "bot.py")
 
     gameboard = Gameboard()
     # for this to work u should have virtual enviroment in ProjektZespolowy main directiory (same level as ProjektZespolowy, Simulator, tron_app etc)
     # example: */ProjektZespolowy/venv/bin/python
     # this might change
-    bot_1 = pexpect.spawn(f"{PYTHON_INTERPRETER_DIR} {BOT_DIR} bot_1")
-    bot_2 = pexpect.spawn(f"{PYTHON_INTERPRETER_DIR} {BOT_DIR} bot_2")
+    bot_1 = pexpect.spawn(f"{PYTHON_INTERPRETER_DIR} {BOT_PY_DIR} bot_1")  # python bot
+    # bot_2 = pexpect.spawn(f"{PYTHON_INTERPRETER_DIR} {BOT_DIR} bot_2") # python bot
+    bot_2 = pexpect.spawn(f"{NODE_INTERPRETER_DIR} {BOT_JS_DIR} bot_2")  # js bot
     for i in range(20):
         row, col = whole_seq(bot_1)  # sequence for bot1(get position and direction, send map to bot and mark move)
-        if gameboard.check_if_colision(row,col):
+        if gameboard.check_if_colision(row, col):
             print("Colision")
             break
         else:
             gameboard.set_on_position(row, col, 1)  # mark returned position
 
         row, col = whole_seq(bot_2)  # sequence for bot2(get position and direction, send map to bot and mark move)
-        if gameboard.check_if_colision(row,col):
+        if gameboard.check_if_colision(row, col):
             print("Colision")
             break
         else:
@@ -42,6 +46,3 @@ def run():
 
     bot_1.close()
     bot_2.close()
-
-if "__main__":
-    run()
